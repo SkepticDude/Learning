9cfc25b0e1a90a943a363d0cec93c91821f00e31
Niranjan Wagh <68341131+SkepticDude@users.noreply.github.com>
2022-11-08 15:15:10 +0530

    Manually creating and linking nodes
    This is only for understanding. More robust Linked List can be found here : https://github.com/SkepticDude/Learning/blob/main/Python/LinkedList.py

--- Python/LinkedListManual.py
+++ Python/LinkedListManual.py
@@ -3,12 +3,13 @@ class Node:
         self.data = data
         self.nextNode = nextNode
 
-
+# Node Creation
 node1 = Node(1)
 node2 = Node(2)
 node3 = Node(6)
 node4 = Node(24)
 
+# Node Linking together
 node1.nextNode = node2
 node2.nextNode = node3
 node3.nextNode = node4
